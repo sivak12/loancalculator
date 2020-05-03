15c65337638db6e42ddb05a9d2cfbd5e0bad842f
Sivakumaran E <sivakumaran.e@gmail.com>
2020-05-03 21:38:52 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
 
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -23,13 +31,18 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * caculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * caculatedPayments) - principle).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers.');
   }
-  e.preventDefault();
 }
 
 function showError(error) {
+
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
 
   const errorDiv = document.createElement('div');
 
